cd3e3600aafecd021d79a37afd2f54f9d62415f5
BalaajRaza <balaajraza2135@gmail.com>
2024-10-09 01:13:45 +0500

    Grid Implemented
    Till now I have implemented a flexible grid of any given size using a multidimensional linked list

---
+++ game.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "grid.h"
+using namespace std;
+
+int main(){
+    Grid grid(15,15);
+    grid.display();
+}
---
+++ grid.h
@@ -0,0 +1,44 @@
+#ifndef GRID_H
+#define GRID_H
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    char val;
+    int x;
+    int y;
+    Node* left;
+    Node* right;
+    Node* up;
+    Node* down;
+
+    Node(char v , int X , int Y){
+        val = v;
+        x = X;
+        y = Y;
+        left = NULL;
+        right = NULL;
+        up = NULL;
+        down = NULL;
+    }
+};
+
+class Grid{
+    Node* head;
+    int rows;
+    int cols;
+    public:
+    Grid(int r , int c);
+    void initializeGrid();
+    void insert(int x, int y , char v);
+    char getValue(int x, int y);
+    void setValue(int x, int y , char v);
+    Node* getNode(int x, int y);
+    void display();
+
+
+};
+
+#endif 
